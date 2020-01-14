b7b354b72ff4d14bcdb21e10d16d3836e6996477
unknown <takeit2sy@pusan.ac.kr>
2020-01-14 16:52:53 +0900

    csv

--- csv01.py
+++ csv01.py
@@ -5,13 +5,32 @@ conn = pymongo.MongoClient('192.168.99.100', 32766)
 db   = conn.get_database("db1")
 coll = db.get_collection("exam1")
 
-f = open('./resources/exam1.csv', 'r', encoding='utf-8')
-rdr = csv.reader(f)
+f    = open('./resources/exam1.csv', 'r', encoding='utf-8')
+rdr  = csv.reader(f)
 next(rdr, None) # 컬럼 skip
 
+# for line in rdr:
+#     # print(type(line))
+#     # print(line)
+#     dict1          = dict()
+#     dict1[line[0]] = line
+#     print(dict1)
+#     coll.insert_one(dict1)
+
+
 for line in rdr:
-    print(type(line))
-    print(line)
-    dict1          = dict()
-    dict1[line[0]] = line
+    dict1 = dict()    
+    for idx, val in enumerate(line):
+        dict1[column[idx]] = val
+
     coll.insert_one(dict1)
+conn.close()
+
+# dict1 = {
+#         kabir : [
+#             'Kabir Vish', 
+#             'Abhinav Neel', 
+#             'male', '28', 
+#             '12', '0.1'
+#             ]
+#         }
