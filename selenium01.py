d225594ecc7b874cd92c58f363caa65654ebbf81
unknown <takeit2sy@pusan.ac.kr>
2020-01-15 10:41:33 +0900

    스크립트 페이지 크롤링 - 터미널에 정보 띄우기

--- selenium01.py
+++ selenium01.py
@@ -1,22 +1,24 @@
-# `pip install selenium`
-from selenium import webdriver
+# 로그인 이후 페이지 들어가서 크롤링 하기
+# https://chromedriver.chromium.org/downloads
+from selenium import webdriver # pip install selenium
 options     = webdriver.ChromeOptions()
-# options.add_argument('headless') # 화면 출력 안되게
+    # options.add_argument('headless') # 화면 출력 안되게
 options.add_argument("window-size=1920x1080")
 
 driver      = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
             
 driver.get("http://ihongss.com/webboard")
 
+# 원하는 페이지 -F12- Copy-copy selector
 driver.find_element_by_name("email").send_keys('20191216') 
 driver.find_element_by_name("pw").send_keys('20191216')
     # by_name("html의 name값").send_keys(원하는값)
 driver.find_element_by_css_selector('#form1 > div:nth-child(4) > input').click() 
     # by_css_selector('#포함!! copy selector한값').click()  
 
-# 원하는 페이지 -F12- Copy-copy selector
-
-# selector
-#form1 > div:nth-child(4) > input
 # xpath
-# driver.find_element_b
+
+
+driver.get("http://daum.net") # 1st로그인 이후의 페이지
+html    = driver.page_source
+soup    = BeautifulSoup(html, 'html_parser')
---
+++ selenium02.py
@@ -0,0 +1,21 @@
+# 스크립트 페이지에서 크롤링 하기
+from selenium import webdriver
+import time
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+options.add_argument("disable-gpu")   
+options.add_argument("lang=ko_KR")    
+options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 
+
+driver = webdriver.Chrome('./chromedriver.exe', 
+    options=options)
+
+driver.get("https://datalab.naver.com/shoppingInsight/sCategory.naver")
+
+time.sleep(3) # 3초 스크립트 정보가져오도록 기다리기
+tag = driver.find_element_by_class_name('rank_top1000_list') \
+    .find_elements_by_tag_name("li")
+
+for tmp in tag:
+    print(tmp.text.split("\n")) 
