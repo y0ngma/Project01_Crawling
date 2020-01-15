458dede4dfb6c559638be1b0605d6130b5af06f8
unknown <takeit2sy@pusan.ac.kr>
2020-01-15 16:40:47 +0900

    mongo->oracle 넣을때 둘다 켜야함.

--- mongo02.py
+++ mongo02.py
@@ -1,16 +1,33 @@
-import requests
-import json
+
 import pymongo
+import cx_Oracle as oci
 
-conn = pymongo.MongoClient('192.168.99.100', 32766)
-db = conn.get_database('db1')
+conn_o = oci.connect('admin/1234@192.168.99.100:32764/xe', encoding='utf-8')
+cursor = conn_o.cursor()
+
+conn_m = pymongo.MongoClient('192.168.99.100', 32766)
+db = conn_m.get_database('db1')
 coll   = db.get_collection('p20200115')
-
-# dict1   = {"id":"a", "name":"abc", "age":31}
-# coll.insert_one(dict1)
+    # dict1   = {"id":"a", "name":"Abc", "age":31}
+    # coll.insert_one(dict1)
+    # dict2   = {"id":"b", "name":"aBc", "age":32}
+    # coll.insert_one(dict2)
+    # dict3   = {"id":"c", "name":"abC", "age":33}
+    # coll.insert_one(dict3)
 
 # SELECT*FROM p20200115 WHERE ID="a"
-data1   = coll.find()
+data1   = coll.find({}, {'_id':False})
+for tmp in data1:
+    print(tmp)
+    sql='''
+    INSERT INTO TABLE1(NO, ID, NAME, AGE)
+    VALUES(SEQ_TABLE1_NO.nextval, :id, :name, :age)
+    '''
+    cursor.execute(sql, tmp)
+    conn_o.commit()
+
+conn_o.close() # 오라클 연결 끊기
+conn_m.close() # 몽고 연결끊기
 
 # SELECT*FROM p20200115 WHERE ID="a"
 data2   = coll.find({'id':'a'})
@@ -33,7 +50,10 @@ data6   = coll.find({"age":{"$gt":10, "$lte":30}})
 data7   = coll.find().count()
 print(data7) # 반복문 x, 딕셔너리 아님
 
-# SELECT*FROM TABLE WHERE ID='a' OR NAME='b'
-data6   = coll.find({"$or":[{"id":'a', "name":'b'}]})
+# SELECT * FROM TABLE WHERE ID='a' OR NAME='b'
+data8  = coll.find({'$or':[{"id":'a'},{"name":'b'}]})
 
-
+#SELECT * FROM p202001015 id=a or name in (b)
+data9 = coll.find({'$or':[{"id":'a'},{'name':{'$in':['b']}} ] }) 
+for tmp in data9:
+    print(tmp)
