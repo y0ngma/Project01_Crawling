1fc7a69ce12c1b420e4f36927623369e0cf451ff
ohchangsuk <ocs13@ks.ac.kr>
2020-01-17 19:00:58 +0900

    117

---
+++ MiniProject01/test_changsuk.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+import time
+import pymongo
+import cx_Oracle as oci
+import requests
+import json
+
+conn=pymongo.MongoClient('192.168.99.100',32766)#몽고 연결
+db=conn.get_database('team') 
+coll=db.get_collection('all')
+
+######웹을 불러서 값얻어내기#######################
+
+options=webdriver.ChromeOptions()
+# options.add_argument('headless')
+options.add_argument("disable-gpu")   
+options.add_argument("lang=ko_KR")    
+options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 
+driver=webdriver.Chrome('./chromedriver.exe',chrome_options=options)
+# 1,시작 11월30일 00시
+
+# d 1w
+
+
+# 시작 [1일 증가,[ 옆으로 한칸, 저장, 1시간증가, 옆으로 한칸 저장]]
+for d in range (1,32,1):
+    for h in range(0,24,1):
+        url="https://datalab.naver.com/keyword/realtimeList.naver?datetime=2018-12-{:0>2d}T{:0>2d}%3A00%3A00".format(d,h)
+
+
+        ###########################################################################################################################
+        driver.get(url)
+        time.sleep(1)
+        # 2, 1일 증가
+        try:
+            driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/div/a[2]/span').click()#옆으로 한칸 클릭(50대이상 보이게)
+            day=driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div[1]/a[3]/span[1]')#날짜 변수
+            time1=driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div[2]/a[3]/span[1]')
+            yyyy=day.text[0:4]
+            mm=day.text[5:7]
+            dd=day.text[8:10]
+            time2=time1.text[0:2]
+            dbname=yyyy+'-'+mm+'-'+dd
+            
+
+            # coll=db.get_collection(dbname)#db이름 설정
+
+            tag=driver.find_elements_by_class_name('rank_inner')
+            
+            list_all=list()
+            for i in tag:
+                li = i.text.split('\n')
+                list_all.append(li)
+            list_start10=list_all[1:6]
+
+            # print(list_start10)
+            for tmp in range(0,5,1):
+                a=list_start10[tmp]
+                for tmp1 in range(1,len(a),1):
+                    dict1=dict()
+                    dict1["age"]=a[0]
+                    b=a[tmp1].split()
+                    dict1["rank"]=b[0]
+                    dict1["word"]="".join(b[1:])
+                    dict1['year']=yyyy
+                    dict1['month']=mm
+                    dict1['day']=dd
+                    dict1['time']=time2
+                    coll.insert_one(dict1)
+
+            
+            print(dbname,time2,'완료')
+
+        except:
+            pass            
+
+conn.close()        
