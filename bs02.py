afea4186f364dbbe49bdc4e0afca3762c764e62d
unknown <takeit2sy@pusan.ac.kr>
2020-01-17 13:14:21 +0900

    beautiful soap 오답노트

--- bs02.py
+++ bs02.py
@@ -10,19 +10,22 @@ str1= requests.get(url)
 # soup.select   -> 여러개
 
 soup = BeautifulSoup(str1.text, 'html.parser')
-# <div class = 'tit3'>< />
-all_div_tit3 = soup.find_all('div', {"class":"tit3"})
-# soup.select
+    # <div class = 'tit3'>< />과 같이 id: xx지정한값만 가져오기
+all_div_tit3 = soup.find_all('div', {"class":"tit3"}) 
+    # soup.select
 print(all_div_tit3)
-'''
-<div class="tit3">
-    <a href="/movie/bi/mi/basic.nhn?code=135874" title="스파이더맨: 홈커밍">스파이더맨: 홈커밍</a>
-</div>
-'''
+    '''
+    <div class="tit3">
+        <a href="/movie/bi/mi/basic.nhn?code=135874" title="스파이더맨: 홈커밍">스파이더맨: 홈커밍</a>
+    </div>
+    '''
+
 for tmp in all_div_tit3:
-    # <a href='#', id='aaa'>가나다< /a>
-    # print(tmp.find("a").text) # -> 가나다
-    print(tmp.find("a").attrs) # {'key[0]':'#', 'key[1]':'aaa'}
-    # <a id="알고싶은값">가나다</a>
-    title=tmp.find("a").attrs # ->
-    #print(title["title"])    
+    print(tmp.find("a").text) # -> 가나다
+        # <a href='#', id='aaa'>가나다< /a>
+    print(tmp.find("a").attrs) # {'href':'#', 'title':'영화제목'}
+        # <a id="알고싶은값1" id="알고싶은값2">가나다</a>
+        '''
+        {href="/movie/bi/mi/basic.nhn?code=135874" 
+        title="스파이더맨: 홈커밍"}
+        '''
