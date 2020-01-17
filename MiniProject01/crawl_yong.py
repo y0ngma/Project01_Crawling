7b5e8706ab6bb69f97deb75b53512ff7d6c42fe8
unknown <takeit2sy@pusan.ac.kr>
2020-01-17 16:48:58 +0900

    검색어칼럼 순위빼고. 테이블명 날짜로

---
+++ MiniProject01/crawl_yong.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+import time
+import pymongo
+import cx_Oracle as oci
+import requests
+import json
+import pymongo
+
+conn=pymongo.MongoClient('192.168.99.100',32766)
+db=conn.get_database('CS_YH_team') 
+options=webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument("disable-gpu")   
+options.add_argument("lang=ko_KR")    
+options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 
+driver=webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+
+url="https://datalab.naver.com/keyword/realtimeList.naver?datetime=2018-12-01T00%3A00%3A00"
+    # 12.01.00.00
+driver.get(url)
+
+day=driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div[1]/a[3]/span[1]')
+    # 날짜정보xxxx-xx-xx로 만들기
+tmp1 = day.text[:-5]
+tmp2 = tmp1.split('.')
+dbtitle = '-'.join(tmp2)
+print(dbtitle)
+coll=db.get_collection(dbtitle)
+
+time.sleep(1) 
+    # 화면바뀔때마다 클릭 전 시간주기
+driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div[1]/a[2]').click()
+    # 다음 날짜 클릭
+tag=driver.find_elements_by_class_name('rank_inner')
+    # rank_inner 이름의 요소들 모두 담아
+
+for i in tag:
+    li=i.text.split('\n')
+    # print(li)
+    # print(len(li))
+    # print('=========')
+    
+    for tmp in range(1, len(li),1):
+        dic=dict()
+        wordtmp =list()
+        wordvalue = str()
+
+        dic['age']=li[0]
+        dic['rank']=tmp
+
+        wordtmp = li[tmp].split(' ') #list
+        wordvalue =' '.join(wordtmp[1:])
+        dic['word']=wordvalue
+        print(dic['word'])
+        # print(dic)
+        coll.insert_one(dic)
+conn.close()
+
+# 조장 지시 사항
+# 테이블명 날짜로 xxxx-xx-xx
+# 검색어 컬럼에 순위빼서 넣기
+# 전체순위 빼고 50대 까지
+# 다음날 순위 
