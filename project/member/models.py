4c95938725847712e773e071b923b12e1ec459ac
unknown <takeit2sy@pusan.ac.kr>
2020-01-20 16:17:59 +0900

    member-model-member1

--- project/member/models.py
+++ project/member/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-class Table1(models.Model):
+class member1(models.Model): 
     objects = models.Manager() # vs code 오류 제거용
-
-    no      = models.AutoField(primary_key=True) 
-    title   = models.CharField(max_length=200)
-    content = models.TextField()
-    writer  = models.CharField(max_field=50)
-    hit     = models.IntegerField()
-    regdate = models.DateTimeField(auto_now_add=True)
+    # id, pw, name, age, gen, joindate
+    id        = models.CharField(primary_key=True, max_length=200) 
+    pw        = models.CharField(max_length=200)
+    name        = models.CharField(max_length=200)
+   
+    age     = models.IntegerField()
+    gen     = models.IntegerField() # 성별 0,1
+    regdate = models.DateTimeField(auto_now_add=True) 
