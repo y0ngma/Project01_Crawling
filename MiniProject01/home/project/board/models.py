a92b62999dc00e6aba7024adf3bf9594c73f62ec
ohchangsuk <ocs13@ks.ac.kr>
2020-01-20 16:16:43 +0900

    1

--- MiniProject01/home/project/board/models.py
+++ MiniProject01/home/project/board/models.py
@@ -1,14 +1,16 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class board1(models.Model):
-#     object = models.Manager()#vs code 오류 제거용
+# Create your models here.
+class board1(models.Model):
+    objects = models.Manager()#vs code 오류 제거용
 
-#     no     = models.AutoField(primary_key = True)#게시판 글 = 기본키 
-#     age   = models.IntegerField()#글 제목
-#     rank = models.IntegerField()
-#     word = models.TextField()
-#     year = models.IntegerField()
-#     month = models.IntegerField()
-#     day = models.IntegerField()
-#     time = models.IntegerField()
+    no     = models.AutoField(primary_key = True)#게시판 글 = 기본키 
+    gene   = models.CharField(max_length=200)#글 제목
+
+    rank = models.IntegerField()
+    word = models.CharField(max_length=200)
+
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    time = models.IntegerField()
