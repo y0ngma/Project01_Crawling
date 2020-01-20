accf9a45ad342145ffc87525f1f818f5fdcabcc5
unknown <takeit2sy@pusan.ac.kr>
2020-01-20 17:08:06 +0900

    null=true

--- project/member/models.py
+++ project/member/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class member1(models.Model): 
     objects = models.Manager() # vs code 오류 제거용
 
-    id        = models.CharField(primary_key=True, max_length=200) 
+    id        = models.CharField(primary_key=True, max_length=200, null=True) 
     pw        = models.CharField(max_length=200)
-    name        = models.CharField(max_length=200)
+    name      = models.CharField(max_length=200)
    
-    age     = models.IntegerField()
-    gen     = models.IntegerField() # 성별 0,1
-    regdate = models.DateTimeField(auto_now_add=True) 
+    age       = models.IntegerField()
+    gen       = models.IntegerField() # 성별 0,1
+    regdate   = models.DateTimeField(auto_now_add=True) 
