b0baa0ea3607c5c675ffb97ab93270d7b9a608f7
unknown <takeit2sy@pusan.ac.kr>
2020-01-20 16:16:05 +0900

    model-board1

--- project/board/models.py
+++ project/board/models.py
@@ -4,15 +4,12 @@ class Table1(models.Model):
     objects = models.Manager() # vs code 오류 제거용
 
     no    = models.AutoField(primary_key=True) # 자동 번호매기기
-    age   = models.CharField(max_length=200)
-    rank  = models.CharField(max_length=200)
-    word  = models.CharField(max_length=200)
-    month = models.CharField(max_length=200)
-    day   = models.CharField(max_length=200)
-    time  = models.CharField(max_length=200)
+    gene  = models.CharField(max_length=200) #generation 글자
     
-    # rank = models.TextField()
-    # word  = models.CharField(max_field=50)
-    # month = models.DateTimeField(auto_now_add=True)
-    # day = models.DateTimeField(auto_now_add=True)
-    # time = models.DateTimeField(auto_now_add=True)
+    rank  = models.IntegerField() # 순위는 숫자
+    word  = models.CharField(max_length=200)
+    
+    month = models.IntegerField()
+    day   = models.IntegerField()
+    time  = models.IntegerField()
+    
