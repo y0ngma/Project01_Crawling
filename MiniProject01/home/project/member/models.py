907756d7399ab5567750276d37615328451bb30a
ohchangsuk <ocs13@ks.ac.kr>
2020-01-20 16:42:52 +0900

    2

--- MiniProject01/home/project/member/models.py
+++ MiniProject01/home/project/member/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class member1(models.Model):
-    object = models.Manager()#vs code 오류 제거용
+    objects = models.Manager()#vs code 오류 제거용
     id     = models.CharField(primary_key = True, max_length=200)#게시판 글 = 기본키 
     pw   = models.CharField(max_length=200)#글 제목
     name = models.CharField(max_length=200)
+
     age = models.IntegerField()
     gen = models.IntegerField()
     joindate = models.DateField(auto_now_add=True)
