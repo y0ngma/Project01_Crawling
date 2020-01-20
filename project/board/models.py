2ae9b70d97b29e7d676a066a9743fcb56331cb83
unknown <takeit2sy@pusan.ac.kr>
2020-01-20 16:16:53 +0900

    board year

--- project/board/models.py
+++ project/board/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Table1(models.Model):
+class board1(models.Model):
     objects = models.Manager() # vs code 오류 제거용
 
     no    = models.AutoField(primary_key=True) # 자동 번호매기기
@@ -9,6 +9,7 @@ class Table1(models.Model):
     rank  = models.IntegerField() # 순위는 숫자
     word  = models.CharField(max_length=200)
     
+    year  = models.IntegerField()
     month = models.IntegerField()
     day   = models.IntegerField()
     time  = models.IntegerField()
