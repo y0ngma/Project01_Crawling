8fb78bf85730373e400df36e8b1b8ace688abf41
unknown <takeit2sy@pusan.ac.kr>
2020-01-21 18:12:03 +0900

    WHERE NO >100 GROUP BY  DAY

--- MiniProject01/project/board/urls.py
+++ MiniProject01/project/board/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
+path('list',             views.list,           name='list'),
 path('service3',         views.service3,       name='service3'),
 path('service4',         views.service4,       name='service4'),
 
--- MiniProject01/project/board/views.py
+++ MiniProject01/project/board/views.py
@@ -8,32 +8,54 @@ from .models import board1
 @csrf_exempt
 def service3(request):
     if request.method == 'GET':
-        # yr = request.GET.get('year', 2020)
-        # print(type(yr), yr)        
-        # sql = '''
-        #     SELECT
-        #         RANK,WORD,YEAR
-        #     FROM 
-        #         BOARD_BOARD1
-        #     WHERE
-        #         YEAR = %s
-        # '''
-        # cursor.execute(sql, yr)
-        # data = cursor.fetchone()
-        print('=====================get===========')
-        data = 1
-        yrwd = data
-        return render(request, 'board/service3.html', {'yrwd':data})
-    elif request.method == "POST":
-        try :
-            print('========업로드=========')
+        try:
+            yr = request.GET.get('year', 2020)
+
+            print(type(yr), yr)        
+            sql = '''
+                SELECT
+                    RANK,WORD,YEAR,MONTH
+                FROM 
+                    BOARD_BOARD1
+                WHERE
+                    RANK = 1 AND YEAR = %s AND NO < 101
+            '''
+            cursor.execute(sql, yr)
+            data = cursor.fetchall()
+
         except Exception as e:
-            print('=========에러==========')
-        return redirect('/board/service4') 
+            print( '================error===')
+            data = '에러'
+        return render(request, 'board/service3.html', {'yrwd':data})
 
 @csrf_exempt
 def service4(request):
     if request.method == 'GET':
-        return redirect('/board/service4') 
+        
+        data = '키워드'
 
+        return render(request, 'board/service4.html', {'word':data})        
+        # return redirect('/board/service4') 
+   
+@csrf_exempt
+def list(request): 
+    sql = 'SELECT RANK FROM BOARD_BOARD1 WHERE NO <40 GROUP BY RANK'
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(type(data))
+    print(data)
+        # [(, , , ,column렬의 수 만큼 ), (row 행의 수 만큼)]
+        # list.html으로 넘어갈때
+        # list 변수에 data값을, title변수에 회원목록 문자로 해서 넘긴다.
+        # 단 title키의 값은 하나뿐이라 list.html에서 {{title}}가능하고
+        # list키의 값은 회원수만큼이므로 for문 사용했음
+    
+    # sql = 'SELECT WORD FROM BOARD_BOARD1 WHERE RANK=1'
+    sql = 'SELECT DAY FROM BOARD_BOARD1 WHERE NO <40'
+    cursor.execute(sql)
+    data2 = cursor.fetchall()
+    print(type(data2))
+    print(data2)
+
+    return render(request, 'board/list.html', {'tuple':data, 'list':data2})
 
