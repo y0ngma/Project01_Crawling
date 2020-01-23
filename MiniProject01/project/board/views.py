3dadd8abc4079cc3df7a42b290e095f10014053a
unknown <takeit2sy@pusan.ac.kr>
2020-01-23 11:15:47 +0900

    sql 완성

--- MiniProject01/project/board/views.py
+++ MiniProject01/project/board/views.py
@@ -13,104 +13,62 @@ def service3(request):
         MM = [int(i) for i in range(1,13)]
         DD = [int(i) for i in range(1,32)]
         SS = [int(i) for i in range(0,24)]
+        RK = [int(i) for i in range(1,21)]
+    
     # 필터값 설정. 받아온 get name값 담기
-        gn1  = request.GET.get('gene', '10대')#10+20+30+40+50?
-        gn1  = "%" +gn1+ "%" #year1, 2로 구분필요?
-        yy1  = int(request.GET.get('year', 2019))
-        yy2  = int(request.GET.get('year', 2019))
-        mm1  = int(request.GET.get('month', 7))
-        mm2  = int(request.GET.get('month', 7))
-        dd1  = int(request.GET.get('day', 1))
-        dd2  = int(request.GET.get('day', 1))
-        ss1  = int(request.GET.get('time', 4))
-        ss2  = int(request.GET.get('time', 5))
         rk1  = int(request.GET.get('rank', 1))
-        rk2  = int(request.GET.get('rank', 1))
+        rk2  = int(request.GET.get('rank2', 1))
+        gn1  = request.GET.get('gene', '10대') #10+20+30+40+50?
+        gn1  = "%" +gn1+ "%"
+        yy1  = str(request.GET.get('year', 2019))
+        yy2  = str(request.GET.get('year2', 2019))
+        mm1  = str(request.GET.get('month', 7))
+        mm2  = str(request.GET.get('month2', 7))
+        dd1  = str(request.GET.get('day', 1))
+        dd2  = str(request.GET.get('day2', 1))
+        ss1  = str(request.GET.get('time', 4))
+        ss2  = str(request.GET.get('time2', 5))
+
+        filters=[gn1,int(yy1+mm1+dd1+ss1),int(yy2+mm2+dd2+ss2),rk1,rk2]
     # 필터링 시작점
-        filters = [gn1,yy1,yy2,mm1,mm2,dd1,dd2,ss1,ss2,rk1,rk2]
-        print("=====================필터1===========",filters)
         sql = '''
-        SELECT WORD FROM BOARD_BOARD1
-        WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO < 10000) AND
-            GENE LIKE %s AND 
-            (YEAR  >= %s AND YEAR  <= %s) AND
-            (MONTH >= %s AND MONTH <= %s) AND
-            (DAY   >= %s AND DAY   <= %s) AND 
-            (TIME  >= %s AND TIME  <= %s) AND 
-            (RANK  >= %s AND RANK  <= %s)
-        '''
-        cursor.execute(sql, filters)
-        filtered = cursor.fetchall()
-        print("=====================필터된 값=============",filtered)
-        
-    # 카운터
-        sql = '''
-            SELECT WORD,COUNT(*) FROM (
-                SELECT WORD FROM BOARD_BOARD1
-                WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO <= 10000) 
-                AND RANK = 1   
+        SELECT WORD,COUNT(*) FROM (
+                SELECT WORD 
+                FROM BOARD_BOARD1 
+                WHERE 
+                    GENE LIKE %s AND
+                    TO_NUMBER(YEAR||MONTH||DAY||TIME) >= %s AND
+                    TO_NUMBER(YEAR||MONTH||DAY||TIME) <= %s AND
+                    RANK >= %s AND RANK <= %s
                 )
             GROUP BY WORD
             ORDER BY COUNT(*) DESC
-            '''
-    #    
-        # sql = '''
-        #     SELECT WORD,COUNT(*) FROM (
-        #         SELECT WORD FROM BOARD_BOARD1
-        #         WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO < 10000) AND
-        #             GENE LIKE %s AND 
-        #             (YEAR  >= %s AND YEAR  <= %s) AND
-        #             (MONTH >= %s AND MONTH <= %s) AND
-        #             (DAY   >= %s AND DAY   <= %s) AND 
-        #             (TIME  >= %s AND TIME  <= %s) AND 
-        #             (RANK  >= %s AND RANK  <= %s)
-        #         )
-        #     GROUP BY WORD
-        #     ORDER BY COUNT(*) DESC
-        #     '''
-        cursor.execute(sql)
-        data2 = cursor.fetchall()
-        print(type(data2))
-        print("========================",data2)
-
+        '''
+        cursor.execute(sql, filters)
+        filtered = cursor.fetchall()
+        print("=====================필터된 값=======",filtered)
+        print("=====================필터1===========",filters)
         return render(request,'board/service3.html',{'GN':GN,'MM':MM,\
-            "DD":DD,"SS":SS,'filtered':filtered,"filters":filters,'ctwd':data2})
+            "DD":DD,"SS":SS,"RK":RK,'filtered':filtered,\
+                "filters":filters})
    
 
-
 @csrf_exempt
-def list(request): 
-    
+def list(request): # 검색어 최초검색된 날짜, 최종일
     w=request.GET.get('search', "")
     sql="""
         SELECT YEAR, MONTH, DAY, TIME FROM BOARD_BOARD1
-        WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO <= 1000) 
+        WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO <= 1000)AND
+            WORD LIKE %s
         ORDER BY YEAR, MONTH, DAY, TIME ASC
         """
-    cursor.execute(sql)
+    cursor.execute(sql, [w])
     data=cursor.fetchall()
     time1=data[0]
     time2=data[-1]
-    print(time1, time2)
-
-    sql = '''
-        SELECT WORD,COUNT(*) FROM (
-            SELECT WORD FROM BOARD_BOARD1
-            WHERE NO IN (SELECT NO FROM BOARD_BOARD1 WHERE NO <= 1000) 
-            AND RANK = 1   
-            )
-        GROUP BY WORD
-        ORDER BY COUNT(*) DESC
-        '''
-    cursor.execute(sql)
-    data2 = cursor.fetchall()
-    print(type(data2))
-    print(data2)
-
-
-    return render(request,'board/list.html',{'start':time1, 'end':time2, 'ctwd':data2})
+    
+    return render(request,'board/list.html',{'start':time1,'end':time2})
        
-
 @csrf_exempt
 def service4(request):
     if request.method == 'GET':
